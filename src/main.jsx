4465aa536832e6a057c69382026fb762c84bc9f5
Vakarva <151787968+Vakarva@users.noreply.github.com>
2025-01-27 15:39:02 -0800

    Added dev dependencies eslint, @eslint/js, globals

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import Plotly from "plotly.js-dist-min";
 
 const root = document.getElementById("root");
 Plotly.newPlot(
-  root,
-  [
+    root,
+    [
+        {
+            x: [1, 2, 3, 4, 5],
+            y: [1, 2, 4, 8, 16],
+        },
+    ],
     {
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16],
+        margin: { t: 0 },
     },
-  ],
-  {
-    margin: { t: 0 },
-  }
 );
