6721c0e88973bc6d8eaaa8c42cf273f08c7a7a38
Vakarva <151787968+Vakarva@users.noreply.github.com>
2025-01-27 16:15:20 -0800

    Added configuration for eslint for React, React hooks, and JSX

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
 import js from "@eslint/js";
 import globals from "globals";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import reactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 export default [
     js.configs.recommended,
@@ -13,5 +16,18 @@ export default [
                 ...globals.browser,
             },
         },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    },
+    reactRecommended,
+    reactJSXRuntime,
+    {
+        plugins: {
+            "react-hooks": reactHooksPlugin,
+        },
+        rules: reactHooksPlugin.configs.recommended.rules,
     },
 ];
